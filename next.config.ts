e0313edfdcf793f06d3ad2a6e7ffbd9cd465964a
memset0 <memset0@outlook.com>
2025-06-28 11:33:34 +0800

    chore(ci): 使用指定的构建目录名

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 静态导出
   output: "export",
+  distDir: "dist",
+  trailingSlash: true, // Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+
   reactStrictMode: true,
 };
 
