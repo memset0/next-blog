2d6b32e63d11cf37131776084b987d7f351bbfda
memset0 <memset0@outlook.com>
2025-06-28 11:29:50 +0800

    feat(friends): 完善友链数据的处理

--- lib/friends.ts
+++ lib/friends.ts
@@ -14,26 +14,37 @@ export function loadFriendsFromYaml(plainYaml: string): Friend[] {
   const data = yaml.parse(plainYaml);
 
   const friends: Friend[] = [];
-  for (const el of data) {
+  for (const i in data) {
+    const el = data[i];
     const friend: Partial<Friend> = {};
     if (el.name && typeof el.name === "string") {
       friend.name = el.name;
+    } else {
+      friend.name = `unnamed-friend-${i + 1}`;
     }
     if (el.avatar && typeof el.avatar === "string") {
       friend.avatar = el.avatar;
     } else if (el.avatar_url && typeof el.avatar_url === "string") {
       friend.avatar = el.avatar_url;
+    } else {
+      friend.avatar = "#";
     }
     if (el.bio && typeof el.bio === "string") {
       friend.bio = el.bio;
+    } else {
+      friend.bio = "";
     }
     if (el.link && typeof el.link === "string") {
       friend.link = el.link;
+    } else {
+      friend.link = "#";
     }
     if (el.hide && typeof el.hide === "boolean") {
       friend.hide = el.hide;
+    } else {
+      friend.hide = true;
     }
-    friends.push(friend as any as Friend);
+    friends.push(friend as Friend);
   }
 
   return friends;
